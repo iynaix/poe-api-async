9646da580cb5f9c0026ead1acf54edcf368d57a1
Lin Xianyi <iynaix@gmail.com>
2024-10-05 23:10:15 +0800

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -5,40 +5,46 @@
     devenv.url = "github:cachix/devenv";
   };
 
-  outputs = {
-    nixpkgs,
-    devenv,
-    systems,
-    ...
-  } @ inputs: let
-    forEachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
-    devShells =
-      forEachSystem
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              # https://devenv.sh/reference/options/
-              dotenv.disableHint = true;
+  outputs =
+    {
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                dotenv.disableHint = true;
 
-              # setup openssl for reqwest (if used)
-              env = {
-                PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-              };
+                # setup openssl for reqwest (if used)
+                env = {
+                  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+                };
 
-              packages = with pkgs; [
-                cargo-expand
-                cargo-watch
-              ];
+                packages = with pkgs; [
+                  cargo-edit
+                  cargo-expand
+                  cargo-watch
+                ];
 
-              languages.rust.enable = true;
-            }
-          ];
-        };
-      });
-  };
+                languages.rust.enable = true;
+              }
+            ];
+          };
+        }
+      );
+    };
 }
